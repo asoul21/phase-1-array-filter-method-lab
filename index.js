69f80df2f6c23bbde577a60202a3ad7519802912
andrew soulinhavong <soulinhavong@gmail.com>
2024-01-12 06:56:26 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter (driver => driver.toLowerCase() === attribute.toLowerCase())
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter( driver => driver.startsWith(attribute))
+}
+
+function matchName(drivers, attribute) {
+    return drivers.filter(driver => driver.name.toLowerCase() === attribute.toLowerCase())
+}
